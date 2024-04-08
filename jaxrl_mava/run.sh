d88534dfeca83ae5524e40fddfab4fdf7834660a
zaber <20830726+ZaberKo@users.noreply.github.com>
2024-04-08 10:37:50 +0800

    update jax and sb3

--- jaxrl_mava/run.sh
+++ jaxrl_mava/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 sudo apt install libfreetype6-dev libsdl2-dev swig
-mamba env create -f ../jax.yaml -n jaxrl-mava
+cd ../jax
+mamba env create -f jax.yaml -n jaxrl-mava
+pip install -r jaxrl_requirements.txt
